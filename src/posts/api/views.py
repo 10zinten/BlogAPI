1dada19fba773224ecfa1b41d32a64c0eb0d6644
tenzin <ten13zin@gmail.com>
2017-02-07 23:47:28 +0530

    Serialized model data into JSON

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.generics import ListAPIView
 
 
 from posts.models import Post 
+from .serializer import PostSerializer
 
 class PostListAPIView(ListAPIView):
 	queryset = Post.objects.all()
+	serializer_class = PostSerializer
